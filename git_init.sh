20fd35016515ec3497dfdffbf0fee646547c8047
ghecko <ghecko78@gmail.com>
2020-06-30 15:11:51 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.i2c.scan.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.i2c.scan.git
+git remote -v
